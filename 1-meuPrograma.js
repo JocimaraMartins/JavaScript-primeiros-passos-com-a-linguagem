13720ba6dae9753d345a554dc669aa6e9f13fb4f
Jocimara <jocimara1995santos@gmail.com>
2021-08-09 11:09:23 -0300

    primeiros passos com a linguagem

---
+++ 1-meuPrograma.js
@@ -0,0 +1,12 @@
+console.log("Meu Programa em JS: trabalhando com variáveis");
+
+const idade = 26;
+
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade*2);
+console.log(idade/2);
+
+const idadeSomada = 20+4;
+console.log(idadeSomada);
